4089c53c52c43218250d9856556283ba4f67fd00
Seunsanyaa <seunsanyaa@gmail.com>
2020-12-08 10:48:03 +0200

    Changes

--- util/database.js
+++ util/database.js
@@ -1,17 +1,17 @@
-const mongodb= require('mongodb');
-const MongoClient = mongodb.MongoClient;
-
-const mongoConnect =(callback)=>{
-
-
-MongoClient.connect('mongodb+srv://Enemy:nRDtaPio9RnntdUj@privpay.ffr52.mongodb.net/Privpay?retryWrites=true&w=majority')
-    .then(client => {
-        console.log('Connected!');
-        callback(client);
-
-    }).catch(err => {
-        console.log(err);
-});
-};
-
-module.exports=mongoConnect;
+// const mongodb= require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+//
+// const mongoConnect =(callback)=>{
+//
+//
+// MongoClient.connect('mongodb+srv://Enemy:nRDtaPio9RnntdUj@privpay.ffr52.mongodb.net/Privpay?retryWrites=true&w=majority')
+//     .then(client => {
+//         console.log('Connected!');
+//         callback(client);
+//
+//     }).catch(err => {
+//         console.log(err);
+// });
+// };
+//
+// module.exports=mongoConnect;
