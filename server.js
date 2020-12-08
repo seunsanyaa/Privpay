20219fd5b36dede0da87cbfdd460a9cf0c3ce174
Seunsanyaa <seunsanyaa@gmail.com>
2020-12-08 23:37:38 +0200

    few changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 const bodyParser= require('body-parser');
+const axios= require('axios');
 const port = 3000
 const mongoConnect= require('./util/database');
 
@@ -21,16 +22,18 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "API Working" });
-});
+//
+// app.get("/", (req, res) => {
+//     res.json({ message: "API Working" });
+// });
 
 
 const homeRoutes = require('./routes/Home');
 const dashRoutes = require('./routes/Dashboard');
 app.use(homeRoutes);
 app.use(dashRoutes);
+
+
 
 
 
