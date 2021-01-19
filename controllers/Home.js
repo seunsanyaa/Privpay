76a482e753de2f50f7f31e6c20578389959b21b4
Seunsanyaa <seunsanyaa@gmail.com>
2021-01-19 21:50:36 +0200

    Revert "++/--"
    This reverts commit 6cb12a6b4cd621e35b660bd74c2116740e18c315.

--- controllers/Home.js
+++ controllers/Home.js
@@ -31,14 +31,6 @@ exports.forget=('/forget', (req, res) => {
     res.render('pages/forget');
 });
 
-exports.transfer=('/ngnbtc', (req, res) => {
-    res.render('pages/transfer');
-});
-
-exports.transferBtc=('/btcngn', (req, res) => {
-    res.render('pages/transferBtc');
-});
-
 
 exports.bvn=('/bank-verification', (req, res) => {
 
